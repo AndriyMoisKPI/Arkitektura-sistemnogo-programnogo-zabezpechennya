1dcaadf11bb8d0c1517a7f9554a469cee823ae34
AndriyMoisKPI <moisiienko.andrii@lll.kpi.ua>
2026-03-03 07:36:21 +0000

    Update PR23.c

--- PR2/Task3/PR23.c
+++ PR2/Task3/PR23.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int global_init = 10;     // DATA
-int global_uninit;        // BSS
-
-void big_stack() {
-    int big_array1[10000];
-    int big_array2[10000];
-
-    printf("Inside big_stack(): %p %p\n", &big_array1, &big_array2);
-}
+int global_var = 10;
+int global_uninit;
 
 int main() {
-    int local_var = 5;          // STACK
-    int *heap_var = malloc(4);  // HEAP
+    int local_var = 5;
+    int *heap_var = malloc(sizeof(int));
 
-    printf("Text segment (main):      %p\n", main);
-    printf("Data segment:             %p\n", &global_init);
-    printf("BSS segment:              %p\n", &global_uninit);
-    printf("Heap segment:             %p\n", heap_var);
-    printf("Stack top (local_var):    %p\n", &local_var);
-
-    big_stack();   // ← ОСЬ ТУТ викликаємо функцію
+    printf("Text segment: %p\n", main);
+    printf("Data segment: %p\n", &global_var);
+    printf("BSS segment: %p\n", &global_uninit);
+    printf("Heap segment: %p\n", heap_var);
+    printf("Stack segment: %p\n", &local_var);
 
     free(heap_var);
     return 0;
