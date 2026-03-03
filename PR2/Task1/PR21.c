fb3467a8a04017c556339989e4c696cc29556934
AndriyMoisKPI <moisiienko.andrii@lll.kpi.ua>
2026-03-03 07:32:08 +0000

    Add files via upload

--- PR2/Task1/PR21.c
+++ PR2/Task1/PR21.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <limits.h>
 
+int main() {
+    printf("Size of time_t: %zu bytes\n", sizeof(time_t));
+
+    if (sizeof(time_t) == 4) {
+        printf("Likely 32-bit time_t\n");
+        printf("Max value: %ld\n", (long)INT_MAX);
+        printf("Overflow date: 19 January 2038\n");
+    } else if (sizeof(time_t) == 8) {
+        printf("Likely 64-bit time_t\n");
+        printf("Very large range (~292 billion years)\n");
+    }
+
+    return 0;
+}
