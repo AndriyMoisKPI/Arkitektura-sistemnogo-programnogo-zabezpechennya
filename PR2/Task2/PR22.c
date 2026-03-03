f9b28ff98ec3015c3f91babd91830fb77d2671e3
AndriyMoisKPI <moisiienko.andrii@lll.kpi.ua>
2026-03-03 07:34:47 +0000

    Add files via upload

--- PR2/Task2/PR22.c
+++ PR2/Task2/PR22.c
@@ -1,1 +1,38 @@
+/*
+#include <stdio.h>
+int main() {
+    printf("Hello\n");
+    return 0;
+}
+*/
 
+/*
+#include <stdio.h>
+int global_array[1000];
+int main() {
+    printf("Hello\n");
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+int global_array[1000] = {1};
+int main() {
+    printf("Hello\n");
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+void test() {
+    int local_array1[10000];
+    int local_array2[10000] = {1};
+}
+int main() {
+    test();
+    printf("Hello\n");
+    return 0;
+}
+*/
